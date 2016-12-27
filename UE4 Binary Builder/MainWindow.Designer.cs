18421236c49b14e0da68ca0c60ee1bf260994f60
Satheesh <mindfreak2040@gmail.com>
2016-12-27 16:07:46 +0530

    Adding files

---
+++ UE4 Binary Builder/MainWindow.Designer.cs
@@ -0,0 +1,387 @@
+﻿namespace UE4_Binary_Builder
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.bCleanBuild = new System.Windows.Forms.CheckBox();
+            this.bEnableSymStore = new System.Windows.Forms.CheckBox();
+            this.bSignExecutables = new System.Windows.Forms.CheckBox();
+            this.bWithDDC = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.bWithSwitch = new System.Windows.Forms.CheckBox();
+            this.bWithHTML5 = new System.Windows.Forms.CheckBox();
+            this.bWithLinux = new System.Windows.Forms.CheckBox();
+            this.bWithTVOS = new System.Windows.Forms.CheckBox();
+            this.bWithIOS = new System.Windows.Forms.CheckBox();
+            this.bWithAndroid = new System.Windows.Forms.CheckBox();
+            this.bWithMac = new System.Windows.Forms.CheckBox();
+            this.bWithWin32 = new System.Windows.Forms.CheckBox();
+            this.bWithWin64 = new System.Windows.Forms.CheckBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.BuildRocketUE = new System.Windows.Forms.Button();
+            this.AutomationToolBrowse = new System.Windows.Forms.Button();
+            this.AutomationToolPath = new System.Windows.Forms.TextBox();
+            this.bHostPlatformOnly = new System.Windows.Forms.CheckBox();
+            this.mainMenu1 = new System.Windows.Forms.MainMenu(this.components);
+            this.AboutMenu = new System.Windows.Forms.MenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Automation Tool Path:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.bCleanBuild);
+            this.groupBox1.Controls.Add(this.bEnableSymStore);
+            this.groupBox1.Controls.Add(this.bSignExecutables);
+            this.groupBox1.Controls.Add(this.bWithDDC);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Location = new System.Drawing.Point(14, 71);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(371, 178);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Options";
+            // 
+            // bCleanBuild
+            // 
+            this.bCleanBuild.AutoSize = true;
+            this.bCleanBuild.Location = new System.Drawing.Point(222, 109);
+            this.bCleanBuild.Name = "bCleanBuild";
+            this.bCleanBuild.Size = new System.Drawing.Size(90, 20);
+            this.bCleanBuild.TabIndex = 5;
+            this.bCleanBuild.Text = "Clean Build";
+            this.toolTip1.SetToolTip(this.bCleanBuild, "Cleans any previous builds");
+            this.bCleanBuild.UseVisualStyleBackColor = true;
+            // 
+            // bEnableSymStore
+            // 
+            this.bEnableSymStore.AutoSize = true;
+            this.bEnableSymStore.Location = new System.Drawing.Point(222, 83);
+            this.bEnableSymStore.Name = "bEnableSymStore";
+            this.bEnableSymStore.Size = new System.Drawing.Size(146, 20);
+            this.bEnableSymStore.TabIndex = 4;
+            this.bEnableSymStore.Text = "Enable Symbol Store";
+            this.toolTip1.SetToolTip(this.bEnableSymStore, "Whether to add Source indexing to Windows game apps so they can be added to a sym" +
+        "bol server");
+            this.bEnableSymStore.UseVisualStyleBackColor = true;
+            // 
+            // bSignExecutables
+            // 
+            this.bSignExecutables.AutoSize = true;
+            this.bSignExecutables.Location = new System.Drawing.Point(222, 57);
+            this.bSignExecutables.Name = "bSignExecutables";
+            this.bSignExecutables.Size = new System.Drawing.Size(123, 20);
+            this.bSignExecutables.TabIndex = 3;
+            this.bSignExecutables.Text = "Sign Executables";
+            this.toolTip1.SetToolTip(this.bSignExecutables, "Sign the executables produced where signing is available");
+            this.bSignExecutables.UseVisualStyleBackColor = true;
+            // 
+            // bWithDDC
+            // 
+            this.bWithDDC.AutoSize = true;
+            this.bWithDDC.Checked = true;
+            this.bWithDDC.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.bWithDDC.Location = new System.Drawing.Point(222, 31);
+            this.bWithDDC.Name = "bWithDDC";
+            this.bWithDDC.Size = new System.Drawing.Size(96, 20);
+            this.bWithDDC.TabIndex = 2;
+            this.bWithDDC.Text = "Include DDC";
+            this.toolTip1.SetToolTip(this.bWithDDC, resources.GetString("bWithDDC.ToolTip"));
+            this.bWithDDC.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.bWithSwitch);
+            this.groupBox2.Controls.Add(this.bWithHTML5);
+            this.groupBox2.Controls.Add(this.bWithLinux);
+            this.groupBox2.Controls.Add(this.bWithTVOS);
+            this.groupBox2.Controls.Add(this.bWithIOS);
+            this.groupBox2.Controls.Add(this.bWithAndroid);
+            this.groupBox2.Controls.Add(this.bWithMac);
+            this.groupBox2.Controls.Add(this.bWithWin32);
+            this.groupBox2.Controls.Add(this.bWithWin64);
+            this.groupBox2.Controls.Add(this.bHostPlatformOnly);
+            this.groupBox2.Location = new System.Drawing.Point(6, 22);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(210, 149);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Platforms";
+            // 
+            // bWithSwitch
+            // 
+            this.bWithSwitch.AutoSize = true;
+            this.bWithSwitch.Location = new System.Drawing.Point(144, 110);
+            this.bWithSwitch.Name = "bWithSwitch";
+            this.bWithSwitch.Size = new System.Drawing.Size(65, 20);
+            this.bWithSwitch.TabIndex = 9;
+            this.bWithSwitch.Text = "Switch";
+            this.bWithSwitch.UseVisualStyleBackColor = true;
+            // 
+            // bWithHTML5
+            // 
+            this.bWithHTML5.AutoSize = true;
+            this.bWithHTML5.Checked = true;
+            this.bWithHTML5.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.bWithHTML5.Location = new System.Drawing.Point(75, 110);
+            this.bWithHTML5.Name = "bWithHTML5";
+            this.bWithHTML5.Size = new System.Drawing.Size(66, 20);
+            this.bWithHTML5.TabIndex = 8;
+            this.bWithHTML5.Text = "HTML5";
+            this.bWithHTML5.UseVisualStyleBackColor = true;
+            // 
+            // bWithLinux
+            // 
+            this.bWithLinux.AutoSize = true;
+            this.bWithLinux.Checked = true;
+            this.bWithLinux.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.bWithLinux.Location = new System.Drawing.Point(6, 110);
+            this.bWithLinux.Name = "bWithLinux";
+            this.bWithLinux.Size = new System.Drawing.Size(56, 20);
+            this.bWithLinux.TabIndex = 7;
+            this.bWithLinux.Text = "Linux";
+            this.bWithLinux.UseVisualStyleBackColor = true;
+            // 
+            // bWithTVOS
+            // 
+            this.bWithTVOS.AutoSize = true;
+            this.bWithTVOS.Checked = true;
+            this.bWithTVOS.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.bWithTVOS.Location = new System.Drawing.Point(144, 84);
+            this.bWithTVOS.Name = "bWithTVOS";
+            this.bWithTVOS.Size = new System.Drawing.Size(60, 20);
+            this.bWithTVOS.TabIndex = 6;
+            this.bWithTVOS.Text = "TVOS";
+            this.bWithTVOS.UseVisualStyleBackColor = true;
+            // 
+            // bWithIOS
+            // 
+            this.bWithIOS.AutoSize = true;
+            this.bWithIOS.Checked = true;
+            this.bWithIOS.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.bWithIOS.Location = new System.Drawing.Point(75, 84);
+            this.bWithIOS.Name = "bWithIOS";
+            this.bWithIOS.Size = new System.Drawing.Size(47, 20);
+            this.bWithIOS.TabIndex = 5;
+            this.bWithIOS.Text = "iOS";
+            this.bWithIOS.UseVisualStyleBackColor = true;
+            // 
+            // bWithAndroid
+            // 
+            this.bWithAndroid.AutoSize = true;
+            this.bWithAndroid.Checked = true;
+            this.bWithAndroid.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.bWithAndroid.Location = new System.Drawing.Point(6, 84);
+            this.bWithAndroid.Name = "bWithAndroid";
+            this.bWithAndroid.Size = new System.Drawing.Size(71, 20);
+            this.bWithAndroid.TabIndex = 4;
+            this.bWithAndroid.Text = "Android";
+            this.bWithAndroid.UseVisualStyleBackColor = true;
+            // 
+            // bWithMac
+            // 
+            this.bWithMac.AutoSize = true;
+            this.bWithMac.Checked = true;
+            this.bWithMac.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.bWithMac.Location = new System.Drawing.Point(144, 58);
+            this.bWithMac.Name = "bWithMac";
+            this.bWithMac.Size = new System.Drawing.Size(50, 20);
+            this.bWithMac.TabIndex = 3;
+            this.bWithMac.Text = "Mac";
+            this.bWithMac.UseVisualStyleBackColor = true;
+            // 
+            // bWithWin32
+            // 
+            this.bWithWin32.AutoSize = true;
+            this.bWithWin32.Checked = true;
+            this.bWithWin32.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.bWithWin32.Location = new System.Drawing.Point(75, 58);
+            this.bWithWin32.Name = "bWithWin32";
+            this.bWithWin32.Size = new System.Drawing.Size(63, 20);
+            this.bWithWin32.TabIndex = 2;
+            this.bWithWin32.Text = "Win32";
+            this.bWithWin32.UseVisualStyleBackColor = true;
+            // 
+            // bWithWin64
+            // 
+            this.bWithWin64.AutoSize = true;
+            this.bWithWin64.Checked = true;
+            this.bWithWin64.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.bWithWin64.Location = new System.Drawing.Point(6, 58);
+            this.bWithWin64.Name = "bWithWin64";
+            this.bWithWin64.Size = new System.Drawing.Size(63, 20);
+            this.bWithWin64.TabIndex = 1;
+            this.bWithWin64.Text = "Win64";
+            this.bWithWin64.UseVisualStyleBackColor = true;
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.ShowAlways = true;
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip1.ToolTipTitle = "Information";
+            // 
+            // BuildRocketUE
+            // 
+            this.BuildRocketUE.Enabled = false;
+            this.BuildRocketUE.Location = new System.Drawing.Point(14, 255);
+            this.BuildRocketUE.Name = "BuildRocketUE";
+            this.BuildRocketUE.Size = new System.Drawing.Size(371, 52);
+            this.BuildRocketUE.TabIndex = 3;
+            this.BuildRocketUE.Text = "Build";
+            this.toolTip1.SetToolTip(this.BuildRocketUE, "Start build process.");
+            this.BuildRocketUE.UseVisualStyleBackColor = true;
+            this.BuildRocketUE.Click += new System.EventHandler(this.BuildRocketUE_Click);
+            // 
+            // AutomationToolBrowse
+            // 
+            this.AutomationToolBrowse.Location = new System.Drawing.Point(338, 41);
+            this.AutomationToolBrowse.Name = "AutomationToolBrowse";
+            this.AutomationToolBrowse.Size = new System.Drawing.Size(47, 23);
+            this.AutomationToolBrowse.TabIndex = 4;
+            this.AutomationToolBrowse.Text = "...";
+            this.toolTip1.SetToolTip(this.AutomationToolBrowse, "Select AutomationTool.exe from your GitHub Engine\\Binaries\\DotNET\\ folder.\r\nIf it" +
+        "s missing then open UE4.sln and from Solution Explorer right click on Automation" +
+        "Tool (under Programs) and select Build.");
+            this.AutomationToolBrowse.UseVisualStyleBackColor = true;
+            this.AutomationToolBrowse.Click += new System.EventHandler(this.AutomationToolBrowse_Click);
+            // 
+            // AutomationToolPath
+            // 
+            this.AutomationToolPath.Location = new System.Drawing.Point(14, 41);
+            this.AutomationToolPath.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.AutomationToolPath.Name = "AutomationToolPath";
+            this.AutomationToolPath.Size = new System.Drawing.Size(318, 23);
+            this.AutomationToolPath.TabIndex = 0;
+            // 
+            // bHostPlatformOnly
+            // 
+            this.bHostPlatformOnly.AutoSize = true;
+            this.bHostPlatformOnly.Location = new System.Drawing.Point(6, 22);
+            this.bHostPlatformOnly.Name = "bHostPlatformOnly";
+            this.bHostPlatformOnly.Size = new System.Drawing.Size(133, 20);
+            this.bHostPlatformOnly.TabIndex = 0;
+            this.bHostPlatformOnly.Text = "Host Platform Only";
+            this.toolTip1.SetToolTip(this.bHostPlatformOnly, "A helper option to make an installed build for your host platform only, so that y" +
+        "ou don\'t have to disable each platform individually");
+            this.bHostPlatformOnly.UseVisualStyleBackColor = true;
+            this.bHostPlatformOnly.CheckedChanged += new System.EventHandler(this.bHostPlatformOnly_CheckedChanged);
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.AboutMenu});
+            // 
+            // AboutMenu
+            // 
+            this.AboutMenu.Index = 0;
+            this.AboutMenu.Text = "About";
+            this.AboutMenu.Click += new System.EventHandler(this.AboutMenu_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 310);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(323, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Binary builds can be found in Your Engine Folder\\LocalBuilds folder";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 330);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.AutomationToolBrowse);
+            this.Controls.Add(this.BuildRocketUE);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AutomationToolPath);
+            this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Menu = this.mainMenu1;
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UE4 Binary Build Helper";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWindow_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox AutomationToolPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox bHostPlatformOnly;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.CheckBox bWithSwitch;
+        private System.Windows.Forms.CheckBox bWithHTML5;
+        private System.Windows.Forms.CheckBox bWithLinux;
+        private System.Windows.Forms.CheckBox bWithTVOS;
+        private System.Windows.Forms.CheckBox bWithIOS;
+        private System.Windows.Forms.CheckBox bWithAndroid;
+        private System.Windows.Forms.CheckBox bWithMac;
+        private System.Windows.Forms.CheckBox bWithWin32;
+        private System.Windows.Forms.CheckBox bWithWin64;
+        private System.Windows.Forms.CheckBox bEnableSymStore;
+        private System.Windows.Forms.CheckBox bSignExecutables;
+        private System.Windows.Forms.CheckBox bWithDDC;
+        private System.Windows.Forms.Button BuildRocketUE;
+        private System.Windows.Forms.Button AutomationToolBrowse;
+        private System.Windows.Forms.CheckBox bCleanBuild;
+        private System.Windows.Forms.MainMenu mainMenu1;
+        private System.Windows.Forms.MenuItem AboutMenu;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ UE4 Binary Builder/MainWindow.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using UE4_Binary_Builder.Properties;
+
+namespace UE4_Binary_Builder
+{
+    public partial class MainWindow : Form
+    {
+        private static string AUTOMATION_TOOL_NAME = "AutomationTool";
+        private string AutomationExePath = Settings.Default.AutomationPath;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            AutomationToolPath.Text = AutomationExePath;
+
+            if (File.Exists(AutomationExePath) && Path.GetFileNameWithoutExtension(AutomationExePath) == AUTOMATION_TOOL_NAME)
+            {
+                BuildRocketUE.Enabled = true;
+            }
+
+            bHostPlatformOnly.Checked = Settings.Default.SettingHostPlatformOnly;
+            bWithWin64.Checked = Settings.Default.bWithWin64;
+            bWithWin32.Checked = Settings.Default.bWithWin32;
+            bWithMac.Checked = Settings.Default.bWithMac;
+            bWithLinux.Checked = Settings.Default.bWithLinux;
+            bWithHTML5.Checked = Settings.Default.bWithHTML5;
+            bWithAndroid.Checked = Settings.Default.bWithAndroid;
+            bWithIOS.Checked = Settings.Default.bWithIOS;
+            bWithTVOS.Checked = Settings.Default.bWithTVOS;
+            bWithSwitch.Checked = Settings.Default.bWithSwitch;
+
+            bWithDDC.Checked = Settings.Default.bWithDDC;
+            bSignExecutables.Checked = Settings.Default.bSignExes;
+            bEnableSymStore.Checked = Settings.Default.bSymStore;
+            bCleanBuild.Checked = Settings.Default.bCleanBuild;
+        }
+
+        private void bHostPlatformOnly_CheckedChanged(object sender, EventArgs e)
+        {
+            bWithWin64.Enabled = !bHostPlatformOnly.Checked;
+            bWithWin32.Enabled = !bHostPlatformOnly.Checked;
+            bWithMac.Enabled = !bHostPlatformOnly.Checked;
+            bWithLinux.Enabled = !bHostPlatformOnly.Checked;
+            bWithHTML5.Enabled = !bHostPlatformOnly.Checked;
+            bWithAndroid.Enabled = !bHostPlatformOnly.Checked;
+            bWithIOS.Enabled = !bHostPlatformOnly.Checked;
+            bWithTVOS.Enabled = !bHostPlatformOnly.Checked;
+            bWithSwitch.Enabled = !bHostPlatformOnly.Checked;
+        }
+
+        private void BuildRocketUE_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("You are going to build a binary version of Unreal Engine 4. This is a long process and might take time to finish. Are you sure you want to continue? ", "Build Binary Version", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                if (bWithDDC.Checked)
+                {
+                    DialogResult MessageResult = MessageBox.Show("Building Derived Data Cache (DDC) is one of the slowest aspect of the build. You can skip this step if you want to. Do you want to continue with DDC enabled?\n\nPress Yes to continue with build\nPress No to continue without DDC\nPress Cancel to stop build", "Warning", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+                    
+                    switch (MessageResult)
+                    {
+                        case DialogResult.No:
+                            bWithDDC.Checked = false;
+                            break;
+                        case DialogResult.Cancel:
+                            return;
+                        default:
+                            break;
+                    }                    
+                }
+                string CommandLineArgs = "BuildGraph -target=\"Make Installed Build Win64\" -script=Engine/Build/InstalledEngineBuild.xml -set:WithDDC=" + GetConditionalString(bWithDDC.Checked) + " -set:SignExecutables=" + GetConditionalString(bSignExecutables.Checked) + " -set:EnableSymStore=" + GetConditionalString(bEnableSymStore.Checked);                
+
+                if (bHostPlatformOnly.Checked)
+                {
+                    CommandLineArgs += " -set:HostPlatformOnly=true";
+                }
+                else
+                {
+                    CommandLineArgs += String.Format(" -set:WithWin64={0} -set:WithWin32={1} -set:WithMac={2} -set:WithAndroid={3} -set:WithIOS={4} -set:WithTVOS={5} -set:WithLinux={6} -set:WithHTML5={7} -set:WithSwitch={8}", 
+                        GetConditionalString(bWithWin64.Checked),
+                        GetConditionalString(bWithWin32.Checked),
+                        GetConditionalString(bWithMac.Checked),
+                        GetConditionalString(bWithAndroid.Checked),
+                        GetConditionalString(bWithIOS.Checked),
+                        GetConditionalString(bWithTVOS.Checked),
+                        GetConditionalString(bWithLinux.Checked),
+                        GetConditionalString(bWithHTML5.Checked),
+                        GetConditionalString(bWithSwitch.Checked));
+                }
+
+                if (bCleanBuild.Checked)
+                {
+                    CommandLineArgs += " -Clean";
+                }
+                ProcessStartInfo AutomationStartInfo = new ProcessStartInfo();
+                AutomationStartInfo.FileName = AutomationExePath;
+                AutomationStartInfo.Arguments = CommandLineArgs;
+                Process.Start(AutomationStartInfo);
+                Application.Exit();
+            }
+        }
+
+        private void AutomationToolBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog NewFileDialog = new OpenFileDialog();
+            NewFileDialog.Filter = "exe file (*.exe)|*.exe";
+
+            if (NewFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                AutomationExePath = NewFileDialog.FileName;
+                AutomationToolPath.Text = AutomationExePath;
+                if (Path.GetFileNameWithoutExtension(AutomationExePath) == AUTOMATION_TOOL_NAME)
+                {
+                    BuildRocketUE.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("This is not Automation Tool. Please select AutomationTool.exe", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private string GetConditionalString(bool bCondition)
+        {
+            return bCondition ? "true" : "false";
+        }
+
+        private void MainWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Settings.Default.AutomationPath = AutomationExePath;
+
+            Settings.Default.SettingHostPlatformOnly = bHostPlatformOnly.Checked;
+            Settings.Default.bWithWin64 = bWithWin64.Checked;
+            Settings.Default.bWithWin32 = bWithWin32.Checked;
+            Settings.Default.bWithMac = bWithMac.Checked;
+            Settings.Default.bWithLinux = bWithLinux.Checked;
+            Settings.Default.bWithHTML5 = bWithHTML5.Checked;
+            Settings.Default.bWithAndroid = bWithAndroid.Checked;
+            Settings.Default.bWithIOS = bWithIOS.Checked;
+            Settings.Default.bWithTVOS = bWithTVOS.Checked;
+            Settings.Default.bWithSwitch = bWithSwitch.Checked;
+
+            Settings.Default.bWithDDC = bWithDDC.Checked;
+            Settings.Default.bSignExes = bSignExecutables.Checked;
+            Settings.Default.bSymStore = bEnableSymStore.Checked;
+            Settings.Default.bCleanBuild = bCleanBuild.Checked;
+            Settings.Default.Save();
+        }
+
+        private void AboutMenu_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("A helper application designed to create binary version of Unreal Engine 4 from source.\n\nCreated by Satheesh (a.k.a ryanjon2040)");
+        }
+    }
+}
