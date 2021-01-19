8b1ba4384465e3a285c00690be63b3e55a6cf829
ryanjon2040 <mindfreak2040@gmail.com>
2021-01-19 14:22:03 +0530

    Update app cast

--- generate_appcast.bat
+++ generate_appcast.bat
@@ -2,10 +2,10 @@
 set tools_version=2.0.0-preview20201108002
 set appcast_path=%UserProfile%\.nuget\packages\netsparkleupdater.tools\%tools_version%\tools\windows\generate_appcast.exe
 set appcast_output=%cd%\UnrealBinaryBuilderUpdater
-set appcast_binary=%cd%\UnrealBinaryBuilder\bin\Release\netcoreapp3.1\win-x64
+set appcast_binary=%cd%\Output
 set appcast_mainexe=%appcast_binary%\UnrealBinaryBuilder.exe
-set appcast_release=https://github.com/ryanjon2040/UE4-Binary-Builder/releases/tag/2.6.1
-set appcast_changelog=https://github.com/ryanjon2040/UE4-Binary-Builder/blob/master/README.md
+set appcast_release=https://github.com/ryanjon2040/UE4-Binary-Builder/releases/download/3.1/UnrealBinaryBuilder.v3.1.zip
+set appcast_changelog=https://raw.githubusercontent.com/ryanjon2040/UE4-Binary-Builder/master/CHANGELOG.md
 set appcast_default_key_path=%localappdata%\netsparkle
 
 set default_option=0
@@ -27,7 +27,7 @@ if %option% == 1 (
     %appcast_path% --verify "%appcast_mainexe%" --signature %input_signature%
 ) else if %option% == 4 (
     if exist %appcast_path% (
-        echo Generating appcast.xml (Debug)
+        echo Generating appcast.xml...
     	%appcast_path% -a "%appcast_output%" -b "%appcast_binary%" -e exe -o windows -u %appcast_release% -l %appcast_changelog% -n "Unreal Binary Builder" --key-path "%appcast_output%\keys"
     	echo Done.
     ) else (
