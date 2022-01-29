7cbb7dcada8b9eef857e701158b1e4ee16f27896
ryanjon2040 <mindfreak2040@gmail.com>
2022-01-29 12:13:43 +0530

    ✨ Update publish bat file

--- publish.bat
+++ publish.bat
@@ -6,5 +6,5 @@ set binary_builder=UnrealBinaryBuilder\UnrealBinaryBuilder.csproj
 if not exist %output_dir% (mkdir %output_dir%)
 rmdir /s /q UnrealBinaryBuilder\bin
 rmdir /s /q UnrealBinaryBuilderUpdater\bin
-dotnet publish -c Release -r win-x64 --output %output_dir% %binary_builder% --self-contained true -p:PublishSingleFile=true
+dotnet publish -c Release -r win-x64 --output %output_dir% %binary_builder% --self-contained true -p:PublishSingleFile=true --force
 pause
